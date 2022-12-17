ecf5719b5886f0a73bcb5218af7b9cf614db8877
Helen <lenapukhova3m96@mail.ru>
2022-12-17 19:52:23 +0300

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Write("Введите любое число (A): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [number];
+Random rnd = new Random();
+
+for (int i = 0; i < number; i++)
+{
+    array[i] = new Random().Next(number + 1);
+    Console.Write(array[i] + "; ");
+}
+
+ Console.WriteLine();
